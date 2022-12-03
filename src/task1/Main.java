1e39fa88abaff2ec8c87e4c2b2685ae75597e8f4
Jon1k <zenaserdykov2004@gmail.com>
2022-12-03 17:02:08 +0200

    ahahah

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String args[]) {
         AbstractTramRoute.test();
         System.out.println("Something");
-        System.out.println("lol");
     }
 
 }

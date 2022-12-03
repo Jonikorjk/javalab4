035014cdc5954ee08956ac6021577b458835805e
Jon1k <zenaserdykov2004@gmail.com>
2022-12-03 16:58:59 +0200

    testing1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String args[]) {
         AbstractTramRoute.test();
         System.out.println("Something");
+        System.out.println("something gigi, i just trying to do merge request guys)");
     }
 
 }
